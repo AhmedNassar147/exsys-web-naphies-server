8d783a622549fc90988e65feb1c3296d3c880b85
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2023-06-19 11:06:47 +0300

    work on build-exsys-naphies-bridge

--- src/nodeHelpers/getCurrentDate.mjs
+++ src/nodeHelpers/getCurrentDate.mjs
@@ -3,14 +3,29 @@
  * Helper: `getCurrentDate`.
  *
  */
+const formalizeValue = (value) => {
+  const strValue = value.toString();
+  return value < 10 ? `0${strValue}` : strValue;
+};
+
 const getCurrentDate = () => {
-  const date = new Date();
-  const [dateString, time] = date.toLocaleString().split(", ");
-  const [month, day, year] = dateString.split("/");
+  const ts = Date.now();
+
+  const date = new Date(ts);
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const month = formalizeValue(date.getMonth() + 1);
+  const hours = formalizeValue(date.getHours());
+  const minutes = formalizeValue(date.getMinutes());
+  const seconds = formalizeValue(date.getSeconds());
+  const amOrPm = date.getHours() < 12 ? "am" : "pm";
+
+  const time = `${hours}:${minutes}:${seconds} ${amOrPm}`;
+  const dateString = `${day}-${month}-${year}`;
 
   return {
-    time: time.toLowerCase(),
-    dateString: `${day}-${month}-${year}`,
+    time,
+    dateString,
   };
 };
 
