9df0d541be29c2f4b1f4f67a477091db813f9630
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2023-06-19 15:15:06 +0300

    work on build-exsys-naphies-bridge

--- src/nphiesHelpers/extraction/mapEntriesAndExtractNeededData.mjs
+++ src/nphiesHelpers/extraction/mapEntriesAndExtractNeededData.mjs
@@ -9,19 +9,16 @@ const mapEntriesAndExtractNeededData = (
 ) => {
   const { entry, id: bundle_id } = nphiesResponse || {};
   if (Array.isArray(entry) && entry.length && extractionFunctionsMap) {
-    const result = entry.reduce((acc, { resource }) => {
+    const result = [...entry].reduce((acc, { resource }) => {
       const { resourceType } = resource;
       const extractionFn = extractionFunctionsMap[resourceType];
 
       if (extractionFn) {
-        if (!acc) {
-          acc = {};
-        }
         acc[resourceType] = extractionFn(resource);
       }
 
       return acc;
-    });
+    }, {});
 
     return {
       bundleId: bundle_id,
