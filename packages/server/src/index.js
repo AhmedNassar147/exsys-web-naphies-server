c936de646fb04de3e97ed1b61efe14fdc5dfe583
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2023-07-16 14:11:33 +0300

    work on claim api

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -10,6 +10,7 @@ import { restartProcess, RESTART_SERVER_MS } from "@exsys-web-server/helpers";
 import { SERVER_PORT, FILES_ENCODING_LIMIT } from "./constants.mjs";
 import createEligibilityMiddleware from "./middlewares/eligibility/index.mjs";
 import createPreauthorizationMiddleware from "./middlewares/preauthorization/index.mjs";
+import createClaimMiddleware from "./middlewares/claim/index.mjs";
 import stopTheProcessIfCertificateNotFound from "./helpers/stopTheProcessIfCertificateNotFound.mjs";
 
 (async () => await import("./polls/index.mjs"))();
@@ -25,6 +26,7 @@ import stopTheProcessIfCertificateNotFound from "./helpers/stopTheProcessIfCerti
   app.use(bodyParser.raw({ limit: FILES_ENCODING_LIMIT }));
   app.use("/eligibility", createEligibilityMiddleware(app));
   app.use("/preauth", createPreauthorizationMiddleware(app));
+  app.use("/claim", createClaimMiddleware(app));
 
   const res = app.listen(SERVER_PORT, () =>
     console.log(`app is running on http://localhost:${SERVER_PORT}`)
