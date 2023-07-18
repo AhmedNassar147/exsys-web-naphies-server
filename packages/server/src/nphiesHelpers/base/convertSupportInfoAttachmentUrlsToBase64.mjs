3e39d2716d9d0f5788514aa2845ff8aaca8f55dd
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2023-07-18 16:55:20 +0300

    work on preauth and claim dunctions

--- packages/server/src/nphiesHelpers/base/convertSupportInfoAttachmentUrlsToBase64.mjs
+++ packages/server/src/nphiesHelpers/base/convertSupportInfoAttachmentUrlsToBase64.mjs
@@ -8,18 +8,33 @@ import { isArrayHasData } from "@exsys-web-server/helpers";
 import { SUPPORT_INFO_KEY_NAMES } from "../../constants.mjs";
 import convertFileUrlToBase64 from "../../helpers/convertFileUrlToBase64.mjs";
 
-const fixContentType = (contentType) =>
-  contentType
-    ? contentType.includes("image")
-      ? contentType.replace(/\/.+/gm, "/jpeg")
-      : contentType
-    : undefined;
+const { attachment } = SUPPORT_INFO_KEY_NAMES;
+
+const NPHIES_SUPPORTED_IMAGE_EXTENSIONS = ["/jpeg", "/jpg"];
+
+const fixContentType = (contentType) => {
+  if (contentType) {
+    const isImage = contentType.includes("image");
+
+    if (isImage) {
+      contentType = contentType.replace(/\/.+/gm, (value) =>
+        NPHIES_SUPPORTED_IMAGE_EXTENSIONS.includes(value)
+          ? value
+          : NPHIES_SUPPORTED_IMAGE_EXTENSIONS[0]
+      );
+    }
+
+    return contentType;
+  }
+
+  return undefined;
+};
 
 const convertSupportInfoAttachmentUrlsToBase64 = async (supportInfo) => {
   if (isArrayHasData(supportInfo)) {
     return await Promise.all(
       supportInfo.map(async ({ categoryCode, value, contentType, ...item }) => {
-        const isAttachment = categoryCode === SUPPORT_INFO_KEY_NAMES.attachment;
+        const isAttachment = categoryCode === attachment;
 
         const { value } = item;
 
