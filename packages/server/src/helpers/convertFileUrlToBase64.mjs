1d64180c5eea850406548748bd556b2fb019c890
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2023-07-28 15:56:43 +0300

    fix table query pagination

--- packages/server/src/helpers/convertFileUrlToBase64.mjs
+++ packages/server/src/helpers/convertFileUrlToBase64.mjs
@@ -4,26 +4,39 @@
  *
  */
 import axios from "axios";
+import { delayProcess } from "@exsys-web-server/helpers";
 
 const convertFileUrlToBase64 = async (fileUrl, removeBaseData) =>
   await new Promise(async (resolve) => {
-    try {
-      const fileData = await axios.get(fileUrl, {
-        responseType: "arraybuffer",
-      });
+    const wrapper = (n) => {
+      axios
+        .get(fileUrl, {
+          responseType: "arraybuffer",
+        })
+        .then(({ data }) => {
+          let fileBase64 = Buffer.from(data).toString("base64");
 
-      let fileBase64 = Buffer.from(fileData.data).toString("base64");
+          if (removeBaseData && fileBase64) {
+            fileBase64 = fileBase64.replace(/.+base64,/, "");
+          }
 
-      if (removeBaseData && fileBase64) {
-        fileBase64 = fileBase64.replace(/.+base64,/, "");
-      }
+          return resolve(fileBase64);
+        })
+        .catch(async (error) => {
+          const { response } = error || {};
+          const { status } = response || {};
+          const isNotFoundUrl = status === 404;
 
-      return resolve(fileBase64);
-    } catch (error) {
-      const { response } = error || {};
-      const { status } = response || {};
-      return resolve(!(status === 404));
-    }
+          if (n > 0 && typeof status === "undefined") {
+            await delayProcess(1100);
+            wrapper(--n);
+          } else {
+            resolve(!isNotFoundUrl);
+          }
+        });
+    };
+
+    wrapper(7);
   });
 
 export default convertFileUrlToBase64;
