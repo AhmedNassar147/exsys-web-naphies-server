ab7d3910883591b68831cde0bb305e30d26889da
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2023-08-03 13:52:18 +0300

    fix nphies and exsys requests timeout

--- packages/helpers/src/createMappedRequestsArray.mjs
+++ packages/helpers/src/createMappedRequestsArray.mjs
@@ -15,7 +15,7 @@ const createMappedRequestsArray = async ({
 }) => {
   if (isArrayHasData(dataArray)) {
     const configPromises = dataArray
-      .map((item, index) => asyncFn(item, !index ? 0 : index + 6, index))
+      .map((item, index) => asyncFn(item, !index ? 0 : index + 2, index))
       .filter(Boolean)
       .flat();
 
