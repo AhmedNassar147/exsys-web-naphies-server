3481da7d65baa59e02072f63c099e50ce2b4726e
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2023-08-20 17:18:33 +0300

    work on communication

--- packages/server/src/exsysHelpers/fetchPreauthAndClaimCommunicationResponse.mjs
+++ packages/server/src/exsysHelpers/fetchPreauthAndClaimCommunicationResponse.mjs
@@ -5,13 +5,11 @@
  */
 import convertSupportInfoAttachmentUrlsToBase64 from "../nphiesHelpers/base/convertSupportInfoAttachmentUrlsToBase64.mjs";
 import createBaseFetchExsysDataAndCallNphiesApi from "./createBaseFetchExsysDataAndCallNphiesApi.mjs";
-import extractClaimResponseData from "../nphiesHelpers/extraction/extractClaimResponseData.mjs";
 import extractCommunicationData from "../nphiesHelpers/extraction/extractCommunicationData.mjs";
 import extractCoverageEntryResponseData from "../nphiesHelpers/extraction/extractCoverageEntryResponseData.mjs";
 import validateSupportInfoDataBeforeCallingNphies from "../nphiesHelpers/base/validateSupportInfoDataBeforeCallingNphies.mjs";
 import createNphiesRequestPayloadFn from "../nphiesHelpers/communication/index.mjs";
 import { EXSYS_API_IDS_NAMES, NPHIES_RESOURCE_TYPES } from "../constants.mjs";
-import createMappedCommunicationRequests from "./createMappedCommunicationRequests.mjs";
 
 const { COVERAGE } = NPHIES_RESOURCE_TYPES;
 
@@ -22,7 +20,6 @@ const {
 
 const extractionFunctionsMap = {
   [COVERAGE]: extractCoverageEntryResponseData,
-  ClaimResponse: extractClaimResponseData,
   Communication: extractCommunicationData,
   CommunicationRequest: extractCommunicationData,
 };
@@ -47,17 +44,17 @@ const createExsysSaveApiParams = ({
 
   return {
     [exsysDataApiPrimaryKeyName]: primaryKey,
+    outcome: status,
     creation_bundle_id: creationBundleId,
     bundle_id: bundleId,
     communication_id: communicationId,
     communication_identifier: communicationIdentifier,
-    communication_status: status,
   };
 };
 
 const createExsysErrorSaveApiBody = (errorMessage) => ({
   nphiesExtractedData: {
-    communicationStatus: "error",
+    outcome: "error",
     issueError: errorMessage,
   },
 });
@@ -78,17 +75,17 @@ const fetchPreauthAndClaimCommunicationResponse = async ({
   exsysQueryApiDelayTimeout,
   nphiesApiDelayTimeout,
 }) => {
-  const { request_type } = requestParams;
+  const { request_type, communicationPk } = requestParams;
 
   const checkExsysDataValidationBeforeCallingNphies =
     validateSupportInfoDataBeforeCallingNphies("communication_payload", true);
 
   return await createBaseFetchExsysDataAndCallNphiesApi({
     exsysQueryApiId: collectExsysClaimOrPreauthCommunicationData,
-    // exsysSaveApiId: saveExsysClaimOrPreauthCommunicationData,
+    exsysSaveApiId: saveExsysClaimOrPreauthCommunicationData,
     requestParams,
     requestMethod: "GET",
-    printFolderName: `communication/${request_type}`,
+    printFolderName: `communication/${request_type}/${communicationPk}`,
     exsysDataApiPrimaryKeyName: "communication_pk",
     createResultsDataFromExsysResponse,
     createNphiesRequestPayloadFn,
