19cd6021b84ae0222ed393a5590fa44760b48ef7
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2024-01-16 11:19:58 +0200

    add pupteer

--- packages/server/src/helpers/scrapNphiesSiteData.mjs
+++ packages/server/src/helpers/scrapNphiesSiteData.mjs
@@ -9,6 +9,8 @@ import { writeResultFile } from "@exsys-web-server/helpers";
 const nphiesPageUrl =
   "https://sso.nphies.sa/auth/realms/sehaticoreprod/protocol/openid-connect/auth?client_id=tv-ui&redirect_uri=https%3A%2F%2Fviewer.nphies.sa%2FLightFHIR&state=2f70125f-1c82-41af-b7d0-62461ef7b07b&response_mode=fragment&response_type=code&scope=openid&nonce=28f2911e-f02d-4903-85f2-41d4627c2506";
 
+const ignoredUrlsSubValues = [".svg", ".css", ".png", ".jpg", "recaptcha"];
+
 const scrapNphiesSiteData = async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -20,7 +22,22 @@ const scrapNphiesSiteData = async () => {
   });
 
   const page = await browser.newPage();
-  await page.setViewport({ width: 1080, height: 1024 });
+  await page.setViewport({ width: 1285, height: 890 });
+  const pageData = await page.goto(nphiesPageUrl, { timeout: 100000 });
+
+  await page.type("#username", "halsaggaf@sagaf-eye.com");
+  await page.type("#password", "Hussien@123");
+
+  const url = pageData.url();
+  const text = await pageData.text();
+
+  await writeResultFile({
+    folderName: "nphies_interceptedRequest/json.json",
+    data: {
+      url,
+      text,
+    },
+  });
   await page.setRequestInterception(true);
 
   page.on("request", async (interceptedRequest) => {
@@ -36,27 +53,23 @@ const scrapNphiesSiteData = async () => {
 
     const url = interceptedRequest.url();
 
+    const shouldIgnoreUrl = ignoredUrlsSubValues.some((value) =>
+      url.includes(value)
+    );
+
+    if (shouldIgnoreUrl) {
+      return;
+    }
+
     await writeResultFile({
       folderName: "nphies_interceptedRequest/1.json",
       data: {
         requestUrl: url,
         requestHeaders: interceptedRequest.headers(),
         requestResponse: interceptedRequest.response(),
+        interceptedRequest,
       },
     });
-  });
-
-  const pageData = await page.goto(nphiesPageUrl);
-
-  const url = pageData.url();
-  const text = await pageData.text();
-
-  await writeResultFile({
-    folderName: "nphies_interceptedRequest/json.json",
-    data: {
-      url,
-      text,
-    },
   });
 };
 
