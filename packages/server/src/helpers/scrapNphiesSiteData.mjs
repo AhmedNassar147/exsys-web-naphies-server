5b8cd2822ead783cae30b1d36c7233cddd502fd5
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2024-01-16 15:01:32 +0200

    add pupteer

--- packages/server/src/helpers/scrapNphiesSiteData.mjs
+++ packages/server/src/helpers/scrapNphiesSiteData.mjs
@@ -6,9 +6,6 @@
 import puppeteer from "puppeteer";
 import { writeResultFile, createUUID } from "@exsys-web-server/helpers";
 
-const nphiesPageUrl =
-  "https://sso.nphies.sa/auth/realms/sehaticoreprod/protocol/openid-connect/auth?client_id=tv-ui&redirect_uri=https%3A%2F%2Fviewer.nphies.sa%2FLightFHIR&state=2f70125f-1c82-41af-b7d0-62461ef7b07b&response_mode=fragment&response_type=code&scope=openid&nonce=28f2911e-f02d-4903-85f2-41d4627c2506";
-
 const ignoredUrlsSubValues = [
   ".svg",
   ".css",
@@ -20,68 +17,115 @@ const ignoredUrlsSubValues = [
   "recaptcha",
 ];
 
+const loginButtonSelector = 'input[name="login"]';
+const optFieldSelector = "#otp-number";
+
+const loginPageUrl =
+  "https://sso.nphies.sa/auth/realms/sehaticoreprod/protocol/openid-connect/auth?client_id=tv-ui&redirect_uri=https%3A%2F%2Fviewer.nphies.sa%2FLightFHIR&state=2f70125f-1c82-41af-b7d0-62461ef7b07b&response_mode=fragment&response_type=code&scope=openid&nonce=28f2911e-f02d-4903-85f2-41d4627c2506";
+
+const loginUserName = "nlubad@sagaf-eye.com";
+const loginPassword = "ALsaggaf@20121";
+
+const opt = "";
+
+const submitForm = async (page, submissionSelector, formData) => {
+  const promises = Object.keys(formData).map((fieldSelector) =>
+    page.type(fieldSelector, formData[fieldSelector])
+  );
+
+  await Promise.all([
+    promises,
+    page.waitForNavigation(), // The promise resolves after navigation has finished
+    await page.evaluate(
+      (selector) => document.querySelector(selector).click(), // click the submission button
+      submissionSelector
+    ),
+  ]);
+};
+
 const scrapNphiesSiteData = async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    // args: [
-    //   "--remote-debugging-port=9222",
-    //   "--remote-debugging-address=192.168.13.84",
-    //   "--no-sandbox",
-    // ],
+    defaultViewport: { width: 1080, height: 1024 },
   });
 
   const page = await browser.newPage();
-  // await page.setViewport({ width: 1270, height: 1024 });
-  const pageData = await page.goto(nphiesPageUrl, { timeout: 100000 });
+  await page.goto(loginPageUrl, { timeout: 100000 });
 
-  await page.type("#username", "halsaggaf@sagaf-eye.com");
-  // await page.type("#password", "Hussien@123");
-  await page.setRequestInterception(true);
-
-  const url = pageData.url();
-  const text = await pageData.text();
-
-  await writeResultFile({
-    folderName: "nphies_interceptedRequest/json.json",
-    data: {
-      url,
-      text,
-    },
+  await submitForm(page, loginButtonSelector, {
+    "#username": loginUserName,
+    "#password": loginPassword,
   });
 
-  page.on("request", async (interceptedRequest) => {
-    if (interceptedRequest.isInterceptResolutionHandled()) {
-      return;
-    }
+  const currentPageUrl = page.url();
 
-    interceptedRequest.continue();
+  const isCurrentPageIsOptPage = currentPageUrl.includes("login-actions");
+  const optField = await page.$(optFieldSelector);
+  // await page.waitForSelector(optFieldSelector);
 
-    // if (interceptedRequest.isNavigationRequest()) {
-    //   return;
-    // }
-
-    const url = interceptedRequest.url();
-    const isNavigationRequest = interceptedRequest.isNavigationRequest();
-
-    const shouldIgnoreUrl =
-      // interceptedRequest.isNavigationRequest() ||
-      ignoredUrlsSubValues.some((value) => url.includes(value));
-
-    if (shouldIgnoreUrl) {
-      return;
-    }
-
-    await writeResultFile({
-      folderName: `nphies_interceptedRequest/${createUUID()}.json`,
-      data: {
-        isNavigationRequest,
-        requestUrl: url,
-        requestHeaders: interceptedRequest.headers(),
-        requestResponse: interceptedRequest.response(),
-        interceptedRequest,
-      },
-    });
+  console.log({
+    currentPageUrl,
+    isCurrentPageIsOptPage,
+    optField,
   });
+
+  // await page.type("#otp-number", opt);
+
+  // await writeResultFile({
+  //   folderName: "nphies_interceptedRequest/navigation.json",
+  //   data: {
+  //     url: currentPageUrl,
+  //   },
+  // });
+
+  // await page.type("#otp-number", "OTP");
+  // await page.ent("input[valye=Log In]");
+
+  // const url = pageData.url();
+  // const text = await pageData.text();
+
+  // await writeResultFile({
+  //   folderName: "nphies_interceptedRequest/json.json",
+  //   data: {
+  //     url,
+  //     text,
+  //   },
+  // });
+
+  // await page.setRequestInterception(true);
+  // page.on("request", async (interceptedRequest) => {
+  //   if (interceptedRequest.isInterceptResolutionHandled()) {
+  //     return;
+  //   }
+
+  //   interceptedRequest.continue();
+
+  //   // if (interceptedRequest.isNavigationRequest()) {
+  //   //   return;
+  //   // }
+
+  //   const url = interceptedRequest.url();
+  //   const isNavigationRequest = interceptedRequest.isNavigationRequest();
+
+  //   const shouldIgnoreUrl =
+  //     // interceptedRequest.isNavigationRequest() ||
+  //     ignoredUrlsSubValues.some((value) => url.includes(value));
+
+  //   if (shouldIgnoreUrl) {
+  //     return;
+  //   }
+
+  //   await writeResultFile({
+  //     folderName: `nphies_interceptedRequest/${createUUID()}.json`,
+  //     data: {
+  //       isNavigationRequest,
+  //       requestUrl: url,
+  //       requestHeaders: interceptedRequest.headers(),
+  //       requestResponse: interceptedRequest.response(),
+  //       interceptedRequest,
+  //     },
+  //   });
+  // });
 };
 
 export default scrapNphiesSiteData;
