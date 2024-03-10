1c266aac820859cfa6079ab24c1ede677fd1b9b5
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2024-03-10 17:10:28 +0200

    fix product extensions

--- packages/server/src/exsysToFrontEndHelpers/claimOrPreauth/extraProductExtensionsSentToNphies.mjs
+++ packages/server/src/exsysToFrontEndHelpers/claimOrPreauth/extraProductExtensionsSentToNphies.mjs
@@ -4,6 +4,7 @@
  *
  */
 import { isArrayHasData, toCamelCase } from "@exsys-web-server/helpers";
+import extractNphiesCodeAndDisplayFromCodingType from "../../nphiesHelpers/extraction/extractNphiesCodeAndDisplayFromCodingType.mjs";
 import extractValueFromUrl from "../../nphiesHelpers/extraction/extractValueFromUrl.mjs";
 import { NPHIES_API_URLS } from "../../constants.mjs";
 
@@ -12,7 +13,10 @@ const { BASE_PROFILE_URL } = NPHIES_API_URLS;
 const extraProductExtensionsSentToNphies = (extension) => {
   if (isArrayHasData(extension)) {
     return extension.reduce(
-      (acc, { valueMoney, valueIdentifier, valueBoolean, url }) => {
+      (
+        acc,
+        { valueMoney, valueIdentifier, valueBoolean, url, valueCodeableConcept }
+      ) => {
         const filedName = toCamelCase(
           extractValueFromUrl(url, BASE_PROFILE_URL).replace("-", " ")
         );
@@ -25,6 +29,13 @@ const extraProductExtensionsSentToNphies = (extension) => {
 
         if (valueIdentifier) {
           value = (valueIdentifier.value || "").replace(/Invc-|T_.+|_.+/g, "");
+        }
+
+        if (valueCodeableConcept) {
+          const { code, display } =
+            extractNphiesCodeAndDisplayFromCodingType(valueCodeableConcept);
+
+          value = [display, code].filter(Boolean).join("/");
         }
 
         acc[filedName] = value;
