67062bb5883e7577f187632d7fb41d1475796e77
unknown <anassar@anspire.agency>
2024-09-18 17:53:49 +0300

    add `encounter` section

--- packages/helpers/src/createTimestamp.mjs
+++ packages/helpers/src/createTimestamp.mjs
@@ -5,7 +5,7 @@
  */
 
 const createTimestamp = (date) => {
-  const __date = (Array.isArray(date) ? date : [data]).filter(Boolean);
+  const __date = (Array.isArray(date) ? date : [date]).filter(Boolean);
 
   return new Date(...__date).toISOString();
 };
