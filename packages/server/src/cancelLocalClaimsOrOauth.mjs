17a7d2f05a2f4e2c09979197c1a08ad95bf44dff
unknown <anassar@anspire.agency>
2024-11-27 14:19:36 +0200

    work on nphis claims cancellation

--- packages/server/src/cancelLocalClaimsOrOauth.mjs
+++ packages/server/src/cancelLocalClaimsOrOauth.mjs
@@ -4,22 +4,65 @@
  *
  */
 import {
+  delayProcess,
   findRootYarnWorkSpaces,
+  isArrayHasData,
   readJsonFile,
+  writeResultFile,
 } from "@exsys-web-server/helpers";
 import createMappedClaimOrPreauthCancellation from "./exsysHelpers/createMappedClaimOrPreauthCancellationOrStatusCheck.mjs";
+
+const claimsToBeSentToNphiesPerRequestsMap = 5;
 
 const cancelLocalClaimsOrOauth = async (filePath) => {
   const rootPath = await findRootYarnWorkSpaces();
   const fullFilePath = `${rootPath}/${filePath}`;
-  const claimsOrPreauthData = await readJsonFile(fullFilePath, true);
+  const { data: claims } = await readJsonFile(fullFilePath, true);
 
-  await createMappedClaimOrPreauthCancellation({
-    data: claimsOrPreauthData,
-    authorization: 111111,
-    printValues: true,
-  });
+  let printInfoData = {};
+  let results = [];
+
+  while (claims.length) {
+    const data = claims.splice(0, claimsToBeSentToNphiesPerRequestsMap);
+
+    const { resultsData, printInfo } =
+      await createMappedClaimOrPreauthCancellation({
+        data,
+        authorization: 111111,
+        printValues: false,
+        formatReturnedResults: ({ printInfo, resultsData }) => ({
+          printInfo,
+          resultsData,
+        }),
+      });
+
+    if (isArrayHasData(resultsData)) {
+      results = results.concat(...resultsData);
+    }
+
+    if (printInfo && isArrayHasData(printInfo.data)) {
+      const { folderName, data } = printInfo;
+      const folderData = printInfoData[folderName] || [];
+      printInfoData[folderName] = folderData.concat(...data);
+    }
+
+    if (!!claims.length) {
+      await delayProcess(6000);
+    }
+  }
+
+  const keys = Object.keys(printInfoData);
+
+  while (keys.length) {
+    const [folderName] = keys.splice(0, 1);
+    const data = printInfoData[folderName];
+
+    await writeResultFile({
+      folderName: "MANUAL_CANCELLATION",
+      data: data,
+    });
+  }
 };
 
-const localFilePath = "";
+const localFilePath = "results/new 6.json";
 await cancelLocalClaimsOrOauth(localFilePath);
