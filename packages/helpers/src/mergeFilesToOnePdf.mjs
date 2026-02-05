37f63ad9f882c3e9f17864254e5a0d251a7b44ce
AhmedNassar147 <nassarahmed750@gmail.com>
2026-02-05 14:01:38 +0200

    compress pdf

--- packages/helpers/src/mergeFilesToOnePdf.mjs
+++ packages/helpers/src/mergeFilesToOnePdf.mjs
@@ -9,8 +9,13 @@ import fixContentType, {
 } from "./fixContentType.mjs";
 import isArrayHasData from "./isArrayHasData.mjs";
 import getRemoteFilePathData from "./getRemoteFilePathData.mjs";
+import compressPdfWithGhostscript from "./compressPdfWithGhostscript.mjs";
 
 const { jpeg } = NPHIES_SUPPORTED_IMAGE_EXTENSIONS;
+
+// 1- install Ghostscript https://ghostscript.com/releases/gsdnld.html
+// 2- add C:\Program Files\gs\gs10.05.1\bin to your PATH environment variable
+// 3- check gswin64c --version works in your terminal
 
 const formatFiles = async (filesData) => {
   let parentPdfFileBytes;
@@ -69,19 +74,16 @@ const mergeFilesToOnePdf = (filesData) =>
         ? await PDFDocument.load(parentPdfFileBytes)
         : await PDFDocument.create();
 
-      data.forEach(async ({ ImageType, fileDate }) => {
+      for (const { ImageType, fileDate } of data) {
         if (!ImageType) {
           const fileDocument = await PDFDocument.load(fileDate);
-          const copedFileDocument = await pdfDoc.copyPages(
+          const copiedPages = await pdfDoc.copyPages(
             fileDocument,
-            fileDocument.getPageIndices()
+            fileDocument.getPageIndices(),
           );
-
-          for (let index = 0; index < copedFileDocument.length; index++) {
-            const currentDocumentPage = copedFileDocument[index];
-            pdfDoc.addPage(currentDocumentPage);
-          }
+          for (const p of copiedPages) pdfDoc.addPage(p);
         } else {
+          // If you also support PNG, switch embed method accordingly
           const image = await pdfDoc.embedJpg(fileDate);
           const page = pdfDoc.addPage();
           page.drawImage(image, {
@@ -91,11 +93,19 @@ const mergeFilesToOnePdf = (filesData) =>
             height: page.getHeight(),
           });
         }
-      });
+      }
 
       const pdfBytes = await pdfDoc.save();
+      let _pdfFileBytes = pdfBytes;
 
-      return resolve({ pdfFileBytes: pdfBytes });
+      try {
+        _pdfFileBytes =
+          (await compressPdfWithGhostscript(pdfBytes)) ?? pdfBytes;
+      } catch (error) {
+        console.log("compressPdfWithGhostscript error:", error.message);
+      }
+
+      return resolve({ pdfFileBytes: _pdfFileBytes });
     } catch (error) {
       let _error = error.message || "something wrong when merging the files";
       _error += `  ${filesData.map(({ url }) => url).join("---")}`;
@@ -143,8 +153,8 @@ export default mergeFilesToOnePdf;
 
 // const files = [
 //   {
-//     url: "http://149.102.140.8:7778/reports/rwservlet?server=rep_server_FRHome1+DESTYPE=cache+report=D:\\\\ExsysReports\\\\PBALLRPT.rep+userid=EXSYS_API/EXSYS_API@149.102.140.8:1521/EXSYS+DESTYPE=cache+desformat=pdf+ENVID=EN+LANGUAGE_ID=1+P_EPISODE_INVOICE_NO=I00123/00275+P_EPISODE_NO=1+P_ORG_NO=001+P_PATIENT_FILE_NO=032151",
-//     contentType: "application/pdf",
+//     url: "https://img.freepik.com/free-vector/opposite-adjectives-words-with-big-small_1308-56389.jpg",
+//     contentType: "image/jpg",
 //   },
 //   {
 //     url: "https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c21hbGx8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
@@ -152,5 +162,4 @@ export default mergeFilesToOnePdf;
 //   },
 // ];
 
-// await mergeFilesToOnePdf(files).then(console.log).catch(console.error);
 // const { pdfFileBytes } = await mergeFilesToOnePdf(files);
